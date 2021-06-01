bef7996bf1a91cd84742bb14f5e787dc9df63cc2
Durai Velan <durai.9.velan@stackroute.in>
2021-06-01 11:24:15 +0530

    demo

--- src/main/java/com/hcl/movieapp/watchlist/controller/WatchlistController.java
+++ src/main/java/com/hcl/movieapp/watchlist/controller/WatchlistController.java
@@ -1,21 +1,60 @@
 package com.hcl.movieapp.watchlist.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Restcontroller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hcl.movieapp.watchlist.model.Movies;
+import com.hcl.movieapp.watchlist.service.WatchlistService;
+
+@RestController
+@RequestMapping("/api/v1/movie")
 public class WatchlistController {
 
-
-	// Movie Object
-	@PostMapping
+	@Autowired
+	WatchlistService watchService;
 	
-	// userid
-	@GetMapping
+	@PostMapping("/{userid}/addToWatchList")
+	public ResponseEntity<?> addToWatchList(@RequestBody Movies movie,@PathVariable String userid){
+	
+		if( watchService.addToWatchList(movie, userid))
+			return new ResponseEntity<String>("Movie added to Watch list", HttpStatus.CREATED);
+		else
+			return new ResponseEntity<String>("Movie not added to Watchlist", HttpStatus.CONFLICT);
+	
+	}
+	
+	@GetMapping("/{userid}/getWatchList")
+	public ResponseEntity<?> getWatchList(@PathVariable String userid){
+		List<Movies> movielist = watchService.getAllMovies(userid);
+		
+		if(movielist != null)
+			return new ResponseEntity<List<Movies>>(movielist, HttpStatus.OK);
+		else
+			return new ResponseEntity<String>("Empty Watch list", HttpStatus.NOT_FOUND);
+	
+	}
 	
 	
-	// Movie Object / Movie Id
-	@DeleteMapping
+	@DeleteMapping("/{userid}/deleteMoviefromWatchList")
+	public ResponseEntity<?> dele(@RequestBody Movies movie,@PathVariable String userid){
+		
+		if( watchService.removeMoviefromWatchList(movie, userid))
+			return new ResponseEntity<String>("Movie delete from Watch list", HttpStatus.OK);
+		else
+			return new ResponseEntity<String>("Movie not deleted from Watchlist", HttpStatus.CONFLICT);
 	
+	}
 	
 	
 }
--- src/main/java/com/hcl/movieapp/watchlist/service/WatchlistService.java
+++ src/main/java/com/hcl/movieapp/watchlist/service/WatchlistService.java
@@ -1,11 +1,92 @@
 package com.hcl.movieapp.watchlist.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+import com.hcl.movieapp.watchlist.model.MovieWatchList;
+import com.hcl.movieapp.watchlist.model.Movies;
+import com.hcl.movieapp.watchlist.repository.MovieWatchListRepo;
 
 @Service
 public class WatchlistService {
 
+	@Autowired
+	MovieWatchListRepo movieWatchListRepo;
 	
+	public boolean addToWatchList(Movies movie,String userid) {
+		boolean status = false;
+		
+		if(!movieWatchListRepo.findById(userid).isEmpty()) {
+			
+			if(movieWatchListRepo.findById(userid).isPresent()){
+			
+				MovieWatchList moviewatchlist = movieWatchListRepo.findById(userid).get();
+				List<Movies> movieslist = moviewatchlist.getMovielist();
+				movieslist.add(movie);
+				
+				moviewatchlist.setMovielist(movieslist);
+				movieWatchListRepo.save(moviewatchlist);
+				status = true;
+			}
+		}else {
+			List<Movies> movielist = new ArrayList<>();
+			MovieWatchList moviewatchlist = new MovieWatchList();
+			moviewatchlist.setUserid(userid);
+			
+			movielist.add(movie);
+			
+			moviewatchlist.setMovielist(movielist);
+			movieWatchListRepo.save(moviewatchlist);
+			status = true;
+		}
+		
+		return status;
+	}
 
+	
+	public List<Movies> getAllMovies(String userid){
+		Optional<MovieWatchList> optList = movieWatchListRepo.findById(userid);
+		if(optList.isPresent()) 	
+			return movieWatchListRepo.findById(userid).get().getMovielist();
+		else 
+			return null;
+	}
+	
+	public boolean removeMoviefromWatchList(Movies movie, String userid){
+		boolean status = false;
+		
+		if(!movieWatchListRepo.findById(userid).isEmpty()) {
+			List<Movies> movielist = movieWatchListRepo.findById(userid).get().getMovielist();
+			
+			ListIterator<Movies> listItr = movielist.listIterator();
+			while (listItr.hasNext()) {
+				Movies movies = (Movies) listItr.next();
+				
+				if(movies.getMovieid().equals(movie.getMovieid())) {
+					listItr.remove();
+				}
+				
+			}
+			
+			
+			MovieWatchList moviewatchlist = new MovieWatchList();
+			moviewatchlist.setUserid(userid);
+			moviewatchlist.setMovielist(movielist);
+			movieWatchListRepo.save(moviewatchlist);
+			
+			status = true;
+		}
+		
+		return status;
+	}
+	
+	
+	
 }
